a3e000dd312844af32395c9d06460c0ec51332f7
MargoMarm <116720347+MargoMarm@users.noreply.github.com>
2023-10-06 20:50:29 +0000

    Deploying to gh-pages from @ MargoMarm/cars-rent@1620e42c0a36402c2ce60b6f385a493eb3b32662 🚀

---
+++ static/js/544.df15bd59.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{544:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("p",{children:"THIS IS HOME PAGE"})})}}}]);
+//# sourceMappingURL=544.df15bd59.chunk.js.map
---
+++ static/js/733.66fdd748.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[733],{733:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("p",{children:"THIS IS FAVORITES PAGE"})})}}}]);
+//# sourceMappingURL=733.66fdd748.chunk.js.map
